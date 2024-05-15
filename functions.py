9d1f3a70bcfdc62a19fab6378049ce1fd9cbe2d3
afiab <abidica30@stuy.edu>
2024-05-14 20:30:03 -0400

    wrote and tested sortOneExtension function

--- functions.py
+++ functions.py
@@ -8,18 +8,30 @@ def sortAllExtensions(folderpath):
     put all the folder's extensions into a set
     loop through the set of extensions
         sortOneExtension(folderpath, extension)
+        print a message saying when smth gets moved
     '''
     pass
 
 # function: put specific files into folders by specific file extension
 def sortOneExtension(folderpath, extension, name=''):
     '''
-    create a folder for this extension called name
-        if name is empty, use extension as the name
     loop through the folderpath's contents
         move all the files for that extension into the folder
+        print a message everytime smth is moved
     '''
-    pass
+    if name=='': # if name is empty
+        name = extension # extension is used as default name
+    newLocation = os.path.join(folderpath,name) # location of new folder
+    if not os.path.exists(newLocation): # if the folder doesnt exist alr
+        os.makedirs(newLocation) # make the folder
+    contents = os.listdir(folderpath) # list of folder contents
+    for item in contents: # loop through all the files
+        itemPath = os.path.join(folderpath,item) # current path of the item
+        if os.path.isfile(itemPath): # if this is a file
+            fileExt = item.split('.')[-1] # get the extension
+            if fileExt == extension: # if this is a file we want to filter
+                shutil.move(itemPath, os.path.join(newLocation, item)) # move the file 
+                print(f"{item} has been moved to {newLocation}") # print statmenent to update changes
 
 # function: put files into one folder based on certain keyword in name
 def sortByKeyword(folderpath, keyword, newName=''):
@@ -30,3 +42,11 @@ def sortByKeyword(folderpath, keyword, newName=''):
         
     '''
     pass
+
+def main():
+    folderpath = input("Enter the path of the folder you want to clean: ")
+    '''test one extension'''
+    sortOneExtension(folderpath, "txt","textfiles")
+
+if __name__=="__main__":
+    main()
