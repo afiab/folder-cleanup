ae18903a2a8d323139d4e94be9ffac389c73487e
afiab <abidica30@stuy.edu>
2024-05-15 00:24:34 -0400

    updated sortOneExtension, implemented and tested sortByKeyword

--- functions.py
+++ functions.py
@@ -18,7 +18,7 @@ def sortAllExtensions(folderpath):
         sortOneExtension(folderpath, extension) # sort extensions into subfolders
 
 # function: put specific files into folders by specific file extension
-def sortOneExtension(folderpath, extension, name=''):
+def sortOneExtension(folderpath, extension, subfolder=''):
     '''
     Move all files of a certain extension into a new subfolder
     If no files are moved, and the subfolder did not previously exist, the subfolder is deleted
@@ -26,21 +26,21 @@ def sortOneExtension(folderpath, extension, name=''):
     Parameters:
         folderpath (str): Path of the folder to search through
         extension (str): The extension to filter out, examples: txt, pdf, py
-        name (str): The name of the new subfolder, defaults to the extension being filtered
+        subfolder (str): The name of the new subfolder, defaults to the extension being filtered
     
     Return:
         None
     '''
     # making the subfolder
-    if name=='': # if name is empty
-        name = extension # extension is used as default name
-    newLocation = os.path.join(folderpath,name) # location of new folder
+    if subfolder=='': # if name is empty
+        subfolder = extension # extension is used as default name
+    newLocation = os.path.join(folderpath,subfolder) # location of new folder
     exists = os.path.exists(newLocation) # directory was present before
     if exists: # if the folder exists alr
-        print(f"{name} exists at {newLocation}")
+        print(f"{subfolder} exists at {newLocation}")
     else: # if the folder doesnt exist alr
         os.makedirs(newLocation) # make the folder
-        print(f"{name} has been created at {newLocation}")
+        print(f"{subfolder} has been created at {newLocation}")
     # go through current folder contents and move them
     moved = False
     contents = os.listdir(folderpath) # list of folder contents
@@ -51,28 +51,61 @@ def sortOneExtension(folderpath, extension, name=''):
             if fileExt == extension: # if this is a file we want to filter
                 shutil.move(itemPath, os.path.join(newLocation, item)) # move the file 
                 moved = True # a file has been moved into the subfolder
-                print(f"    {item} -> {name}") # print statmenent to update changes
+                print(f"    {item} -> {subfolder}") # print statmenent to update changes
     # check that stuff has been moved
     if not moved and not exists:
         os.rmdir(newLocation) # remove empty directory
-        print(f"{name} has been removed at {newLocation} because no files were moved")
+        print(f"{subfolder} has been removed at {newLocation} because no files were moved")
 
 # function: put files into one folder based on certain keyword in name
-def sortByKeyword(folderpath, keyword, newName=''):
+def sortByKeyword(folderpath, keyword, subfolder=''):
     '''
-    create a new folder called newName
-        if newName is empty, the folder should be called keyword
-    loop through the contents of folderpath
-        
+    Move all files with a certain keyword in the title into a new subfolder
+    If no files are moved, and the subfolder did not previously exist, the subfolder is deleted
+
+    Parameters:
+        folderpath (str): Path of the folder to search through
+        keyword (str): The keyword to filter filenames by
+        subfolder (str): The name of the new subfolder, defaults to the keyword being filtered
+    
+    Return:
+        None
     '''
-    pass
+    # making the subfolder
+    if subfolder=='': # if name is empty
+        subfolder = keyword # keyword is used as default name
+    newLocation = os.path.join(folderpath,subfolder) # location of new folder
+    exists = os.path.exists(newLocation) # directory was present before
+    if exists: # if the folder exists alr
+        print(f"{subfolder} exists at {newLocation}")
+    else: # if the folder doesnt exist alr
+        os.makedirs(newLocation) # make the folder
+        print(f"{subfolder} has been created at {newLocation}")
+    # go through current folder contents and move them
+    moved = False
+    contents = os.listdir(folderpath) # list of folder contents
+    for item in contents: # loop through all the contents of the folder
+        itemPath = os.path.join(folderpath,item) # current path of the item
+        if os.path.isfile(itemPath): # if this is a file
+            fileName = item[:-len(item.split('.')[-1])-1] # get the filename
+            if keyword in fileName: # if this is a file we want to filter
+                shutil.move(itemPath, os.path.join(newLocation, item)) # move the file 
+                moved = True # a file has been moved into the subfolder
+                print(f"    {item} -> {subfolder}") # print statmenent to update changes
+    # check that stuff has been moved
+    if not moved and not exists:
+        os.rmdir(newLocation) # remove empty directory
+        print(f"{subfolder} has been removed at {newLocation} because no files were moved")
 
 def main():
     folderpath = input("Enter the path of the folder you want to clean: ")
     '''test one extension'''
     # sortOneExtension(folderpath, "txt","textfiles")
     # sortOneExtension(folderpath, "txt")
+    '''test all extensions'''
     # sortAllExtensions(folderpath)
+    '''test sort by specific word'''
+    # sortByKeyword(folderpath,"docx")
 
 if __name__=="__main__":
     main()
